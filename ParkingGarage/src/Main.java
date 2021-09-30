9a29bed7350bf80573c751320befd555b5c68f34
Jon-Moldoff <91075706+Jon-Moldoff@users.noreply.github.com>
2021-09-30 19:16:49 -0400

    Update Main.java

--- ParkingGarage/src/Main.java
+++ ParkingGarage/src/Main.java
@@ -2,7 +2,14 @@
 
 public class Main {
     public static void main(String[] args){
-       Database ParkingData = new Database("Database.txt");
-       
+        
+        String customer = "Example Name";
+        double _RATE = 2; // 2 cents per minute at the parking garage
+        int holderFromDavid = 40; //David wrote the hours and minutes as an Integer, will convert to double
+        double minutesStayed = holderFromDavid; // this value should come from earlier in the code
+        double chargeAmount = (minutesStayed * _RATE)/10; // this will calculate how many cents the customer is charged
+    
+        System.out.println("Total Charge for "+customer+" is: $"+String.format("%.2f",chargeAmount) + 
+                "   Minutes Stayed: " + holderFromDavid);
     }
 }
