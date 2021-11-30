d94dffce07bd74f1c9a6522789e49b3f883c3f58
Jon-Moldoff <91075706+Jon-Moldoff@users.noreply.github.com>
2021-11-29 20:13:09 -0500

    check in/out

--- ParkingGarage/src/GUI/CheckOut.java
+++ ParkingGarage/src/GUI/CheckOut.java
@@ -1,8 +1,9 @@
 package GUI;
 
 
-import GUI.CheckIn;
-
+import GUI.CheckIn.*;
+import static GUI.CheckIn.a;
+import java.util.*;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -14,7 +15,9 @@ import GUI.CheckIn;
  * @author Owner
  */
 public class CheckOut extends javax.swing.JFrame {
-
+    
+    
+    
     /**
      * Creates new form CheckOut
      */ Time T1 = new Time();
@@ -24,7 +27,7 @@ public class CheckOut extends javax.swing.JFrame {
     public CheckOut() {
         initComponents();
    
-        combSpot.setModel(new javax.swing.DefaultComboBoxModel(CheckIn.leave.toArray()));
+        
     }
 
     /**
@@ -37,25 +40,19 @@ public class CheckOut extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         txtDate1 = new javax.swing.JTextField();
-        combSpot = new javax.swing.JComboBox<>();
         btnYes = new javax.swing.JButton();
         btnNo = new javax.swing.JButton();
         btnBack = new javax.swing.JButton();
         btnSubmit = new javax.swing.JButton();
         jTextField2 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        txtDate2 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel1.setText("DATE");
-
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jLabel2.setText("SPOT");
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel3.setText("Are you a subscriber?");
@@ -63,13 +60,6 @@ public class CheckOut extends javax.swing.JFrame {
         txtDate1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtDate1ActionPerformed(evt);
-            }
-        });
-
-        combSpot.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        combSpot.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                combSpotActionPerformed(evt);
             }
         });
 
@@ -103,12 +93,6 @@ public class CheckOut extends javax.swing.JFrame {
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel4.setText("TOTAL");
 
-        txtDate2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtDate2ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -116,20 +100,6 @@ public class CheckOut extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(113, 113, 113)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(combSpot, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(47, 47, 47)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(txtDate2, javax.swing.GroupLayout.DEFAULT_SIZE, 60, Short.MAX_VALUE)
-                                    .addComponent(txtDate1))
-                                .addGap(43, 43, 43))))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(layout.createSequentialGroup()
@@ -147,7 +117,11 @@ public class CheckOut extends javax.swing.JFrame {
                                 .addGap(29, 29, 29)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(btnNo))))))
+                                    .addComponent(btnNo)))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(47, 47, 47)
+                        .addComponent(txtDate1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(410, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -157,13 +131,7 @@ public class CheckOut extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(txtDate1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(txtDate2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(24, 24, 24)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(combSpot, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(106, 106, 106)
+                .addGap(207, 207, 207)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(btnYes)
@@ -195,30 +163,24 @@ public class CheckOut extends javax.swing.JFrame {
         this.checkIN = D1;
     }//GEN-LAST:event_txtDate1ActionPerformed
 
-    private void txtDate2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtDate2ActionPerformed
-        // TODO add your handling code here:
-        int D2 =  Integer.parseInt(txtDate1.getText());
-        
-        this.checkOut = D2;
-    }//GEN-LAST:event_txtDate2ActionPerformed
-
     private void btnSubmitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmitActionPerformed
         // TODO add your handling code here:
         
         // remove spot from unavailable spots array
-        selectedValue = combSpot.getSelectedItem().toString();
-        while (leave.hasNext()) {
-            String x = (String)leave.next();
-            if (x.equals(selectedValue)){
-                leave.remove();
+        // Get spot that the name aligns with from data base, and remove that spot from leave arraylist
+        /*Iterator itr = CheckIn.leave.iterator();
+        while (itr.hasNext()) {
+            String x = (String)itr.next();
+            if (x.equals(CheckIn.selectedValue)){
+                itr.remove();
             }
-        }
+        }*/
         
         
        this.duration =  T1.Duration(checkIN,checkOut);
        
        txtDate1.setText("");
-       txtDate2.setText("");
+       //txtDate2.setText("");
        new start().setVisible(true);
        dispose();
         
@@ -230,10 +192,6 @@ public class CheckOut extends javax.swing.JFrame {
         String duration1 = String.valueOf(duration);
         jTextField2.setText(duration1);
     }//GEN-LAST:event_jTextField2ActionPerformed
-
-    private void combSpotActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_combSpotActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_combSpotActionPerformed
 
     /**
      * @param args the command line arguments
@@ -275,13 +233,10 @@ public class CheckOut extends javax.swing.JFrame {
     private javax.swing.JButton btnNo;
     private javax.swing.JButton btnSubmit;
     private javax.swing.JButton btnYes;
-    private javax.swing.JComboBox<String> combSpot;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField txtDate1;
-    private javax.swing.JTextField txtDate2;
     // End of variables declaration//GEN-END:variables
 }
